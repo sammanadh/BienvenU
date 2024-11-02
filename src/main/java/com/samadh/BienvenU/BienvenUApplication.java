a7c150fcd34aa7eb60f31ddd12702d3b196d240e
samman adhikari <sammanadh@gmail.com>
2024-11-02 14:51:35 +0100

    project intialized

---
+++ src/main/java/com/samadh/BienvenU/BienvenUApplication.java
@@ -0,0 +1,13 @@
+package com.samadh.BienvenU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienvenUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienvenUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samadh/BienvenU/BienvenUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samadh.BienvenU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienvenUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
